3da9d5853559e8dcf434b0d1c860d7a946741086
Pierrick NEAU <pineau@paul-f4Ar5s4.clusters.42paris.fr>
2025-02-12 17:25:10 +0100

    Grace done

---
+++ C/Grace.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#define Grace int main() {FILE *output_file = fopen("Grace_kid.c", "w"); if (output_file) {fprintf(output_file, STR, STR, 10, 34);fclose(output_file);}return (0);}
+#define STR "#include <stdio.h>%2$c%2$c#define Grace int main() {FILE *output_file = fopen(%3$cGrace_kid.c%3$c, %3$cw%3$c); if (output_file) {fprintf(output_file, STR, STR, 10, 34);fclose(output_file);}return (0);}%2$c#define STR %3$c%1$s%3$c%2$c%2$c/*%2$c    Ce programme se replique%2$c*/%2$c%2$cGrace%2$c"
+
+/*
+    Ce programme se replique
+*/
+
+Grace
